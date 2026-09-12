560ab5efba36b2b113c7e2211cba5854e3616916
sidhy4rth <156543361+sidhy4rth@users.noreply.github.com>
2026-09-12 11:46:46 +0530

    Version 1.0.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.rollapp.shared"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
